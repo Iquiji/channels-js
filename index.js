c944e05123d683590ee9cf103f6b340147239908
Iquiji <yt.failerbot.3000@gmail.com>
2020-02-29 18:05:49 +0100

    keywords

--- index.js
+++ index.js
@@ -29,4 +29,38 @@ class UnBufferedChannel {
     }
 }
 exports.UnBufferedChannel = UnBufferedChannel;
-// TODO: BufferedChannel , BoundlessChannel
+// TODO: BoundlessChannel , BufferedChannel
+/*
+class BufferedChannel {
+    constructor(Bufferlength){
+        this.Bufferlength = Bufferlength;
+        this._buffer = [];
+        this._writeQueue = []; // To be Written
+        this._readQueue = []; // To Br Read
+    }
+    write(data){
+        if (this._readQueue.length == 0){
+            let p = new Promise((resolve,reject) => {
+                this._writeQueue.push([resolve,data]);
+            });
+            return p;
+        }else{
+            let read = this._readQueue.shift();
+            read(data);
+            return Promise.resolve();
+        }
+    }
+    read(){
+        if (this._writeQueue.length == 0){
+            let p = new Promise((resolve,reject) => {
+                this._readQueue.push(resolve);
+            })
+            return p;
+        }else{
+            let [written,data] = this._writeQueue.shift();
+            written();
+            return Promise.resolve(data);
+        }
+    }
+}
+*/
