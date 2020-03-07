13245cfdf96fc55c5ba27692d231e643d4487d46
Iquiji <yt.failerbot.3000@gmail.com>
2020-03-07 21:55:09 +0100

    fixed some bug somewhere in buffered read

--- index.js
+++ index.js
@@ -27,10 +27,13 @@ class UnBufferedChannel {
             return Promise.resolve(data);
         }
     }
+    close(){
+        
+    }
     [Symbol.asyncIterator]() {
         return {
             next: async () => {
-                if (this._writeQueue != 0) {
+                if (this._writeQueue.length != 0) {
                     return Promise.resolve({value: await this.read(), done: false});
                 }else{
                     return Promise.resolve({done: true});
@@ -66,31 +69,25 @@ class BufferedChannel {
         }
     }
     read(){
-        if (this._writeQueue.length == 0){
-            if (this._buffer.length < this.Bufferlength && this._buffer.length != 0){
-                return Promise.resolve(this._buffer.shift());
-            }else if(this._buffer.length >= this.Bufferlength){
-                let read_data = this._buffer.shift();
-                let [written,write_data] = this._writeQueue.shift();
-                this._buffer.push(write_data);
-                written();
-                return Promise.resolved(read_data);
-            }else{
-                let p = new Promise((resolve,reject) => {
-                    this._readQueue.push(resolve);
-                })
-                return p;
-            }
-        }else{
-            let [written,data] = this._writeQueue.shift();
+        if(this._buffer.length >= this.Bufferlength && this._writeQueue.length != 0){
+            let read_data = this._buffer.shift();
+            let [written,write_data] = this._writeQueue.shift();
+            this._buffer.push(write_data);
             written();
-            return Promise.resolve(data);
+            return Promise.resolved(read_data);
+        }else if(this._buffer.length != 0){
+            return Promise.resolve(this._buffer.shift());
+        }else{
+            let p = new Promise((resolve,reject) => {
+                this._readQueue.push(resolve);
+            })
+            return p;
         }
     }
     [Symbol.asyncIterator]() {
         return {
             next: async () => {
-                if (this._buffer.length != 0 || this._writeQueue != 0) {
+                if (this._buffer.length != 0 || this._writeQueue.length != 0) {
                     return Promise.resolve({value: await this.read(), done: false});
                 }else{
                     return Promise.resolve({done: true});
@@ -118,19 +115,19 @@ class BoundlessChannel {
         }
     }
     read(){
-            if (this._buffer.length != 0){
-                return Promise.resolve(this._buffer.shift());
-            }else{
-                let p = new Promise((resolve,reject) => {
-                    this._readQueue.push(resolve);
-                })
-                return p;
-            }
+        if (this._buffer.length != 0){
+            return Promise.resolve(this._buffer.shift());
+        }else{
+            let p = new Promise((resolve,reject) => {
+                this._readQueue.push(resolve);
+            })
+            return p;
+        }
     }
     [Symbol.asyncIterator]() {
         return {
             next: async () => {
-                if (this._buffer.length != 0 || this._writeQueue != 0) {
+                if (this._buffer.length != 0 || this._writeQueue.length != 0) {
                     return Promise.resolve({value: await this.read(), done: false});
                 }else{
                     return Promise.resolve({done: true});
